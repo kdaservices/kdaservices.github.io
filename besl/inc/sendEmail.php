4d7a004c861b0b96271b4f145fd5b4cc1e0ced61
Abdul Hanan <Funkyscientist001@gmail.com>
2024-07-16 20:00:43 +0500

    besl preview 2

--- besl/inc/sendEmail.php
+++ besl/inc/sendEmail.php
@@ -1,7 +1,7 @@
 ﻿<?php
 
 // Replace this with your own email address
-$siteOwnersEmail = 'user@website.com';
+$siteOwnersEmail = 'admin@besl.uk';
 
 if ($_POST) {
     $name = trim(stripslashes($_POST['contactName']));
